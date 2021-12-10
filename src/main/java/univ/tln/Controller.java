654a89aff7e16d918fd8ad98ba0cf3ac02a00d71
acho25 <you@example.com>
2021-12-10 23:31:07 +0100

    creation du controller pou la page d'acceuil

---
+++ src/main/java/univ/tln/Controller.java
@@ -0,0 +1,306 @@
+package univ.tln;
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.*;
+
+//import static univ.tln.LoginController.getUsernametxt;
+
+public class Controller implements Initializable {
+    public int i ;
+    private Stage stage;
+    private Scene scene;
+    private Parent root ;
+    int max_cours = 60;
+    private String[][] creneau= new String[max_cours][7];
+    @FXML
+    private AnchorPane scene1 ;
+    @FXML
+    private AnchorPane scene2 ;
+    @FXML
+    private AnchorPane scene3 ;
+    @FXML
+    private AnchorPane scene4 ;
+    @FXML
+    private AnchorPane scene5 ;
+    @FXML
+    private Button btnaccount;
+    @FXML
+    private Button btnmodify;
+    @FXML
+    private Button btnplanning;
+    @FXML
+    private Button btnsettings;
+    @FXML
+    private Button btnabsences;
+    @FXML
+    private Label lblstatus;
+    @FXML
+    private Label lblstatusmini;
+    @FXML
+    private Label iddimanche;
+
+    @FXML
+    private Label idjeudi;
+
+    @FXML
+    private Label idlundi;
+
+    @FXML
+    private Label idmardi;
+
+    @FXML
+    private Label idmercredi;
+
+    @FXML
+    private Label idsamedi;
+
+    @FXML
+    private Label idvendredi;
+    @FXML
+    private TableView listEtudiantId;
+
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label role;
+
+    private static String username;
+
+
+    @Override
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FXMLLoader loader =new FXMLLoader(App.class.getResource("hello-view.fxml"));
+
+        System.out.println(LoginController.user1 + "++++++++++");
+        castdatetime();
+        try {
+            drawrect();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        setcalendar();
+    }
+
+public void handleclicks (ActionEvent e){
+
+    if(e.getSource()==btnaccount){
+        lblstatus.setText("Account");
+        lblstatusmini.setText("/home/account");
+        btnaccount.setBackground(new Background(new BackgroundFill(Color.rgb(63,43,99),CornerRadii.EMPTY, Insets.EMPTY)));
+        scene3.toFront();
+    }
+    else
+    if(e.getSource()==btnmodify){
+        lblstatus.setText("Modify");
+        lblstatusmini.setText("/home/modify");
+        btnaccount.setBackground(new Background(new BackgroundFill(Color.rgb(63,43,99),CornerRadii.EMPTY, Insets.EMPTY)));
+        scene2.toFront();
+    }
+    else if(e.getSource()==btnplanning){
+        lblstatus.setText("Planning");
+        lblstatusmini.setText("/home/planning");
+        btnaccount.setBackground(new Background(new BackgroundFill(Color.rgb(63,43,99),CornerRadii.EMPTY, Insets.EMPTY)));
+        scene1.toFront();
+    }
+    else if(e.getSource()==btnsettings){
+        lblstatus.setText("Settings");
+        lblstatusmini.setText("/home/settings");
+        btnaccount.setBackground(new Background(new BackgroundFill(Color.rgb(63,43,99),CornerRadii.EMPTY, Insets.EMPTY)));
+        scene4.toFront();
+    }
+    else if(e.getSource()==btnabsences){
+
+        lblstatus.setText("Absences");
+        lblstatusmini.setText("/home/absences");
+        btnaccount.setBackground(new Background(new BackgroundFill(Color.rgb(63,43,99),CornerRadii.EMPTY, Insets.EMPTY)));
+        scene5.toFront();
+
+
+    }
+
+
+}
+
+
+
+
+    @FXML
+    Group group = new Group();
+
+    public double houretopxl(double a ){return (a-8)*45.9+47;} //fonction qui donne le pixel de l'heure
+
+    public int datetopxl(int a ){return (a-2)*126+125;} // fonction qui retourne le pixel de la date
+
+    public Calendar getmonday(){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        return c;
+    }
+    public Calendar getsunday(){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+        c.add(Calendar.DATE, 7);
+        return c;
+    }
+
+    public void setcalendar(){
+        Label Tlabel[]=new Label[7];
+        Tlabel[0]= idlundi;
+        Tlabel[1]=idmardi;
+        Tlabel[2]=idmercredi;
+        Tlabel[3]=idjeudi;
+        Tlabel[4]=idvendredi;
+        Tlabel[5]=idsamedi;
+        Tlabel[6]=iddimanche;
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        for (int i = 0; i < 7; i++) {
+            Tlabel[i].setText(df.format(c.getTime()));
+            Tlabel[i].setTextAlignment(TextAlignment.CENTER);
+            Tlabel[i].setTextFill(Color.rgb(255, 255, 255));
+            c.add(Calendar.DATE, 1);
+        }
+
+
+    }
+
+    public void castdatetime() { //fonction qui remplie une liste des creneaux
+         i = 0;
+        DatabaseConnection connection = new DatabaseConnection();
+        Connection connection1 = connection.connectDB();
+
+
+        try {
+
+            Statement statement = connection1.createStatement();
+            PreparedStatement pstmt =connection1.prepareStatement("select DATE_D, DATE_F, BATIMENT,NUM,VIDEO_P,NOM,NATURE from SALLE join CRENEAUX ON(SALLE.ID_S=CRENEAUX.ID_S) join GROUP_COURS ON (CRENEAUX.ID_G=GROUP_COURS.ID_G)join COURS ON (GROUP_COURS.ID_C = COURS.ID_C) where LOGIN =? AND FORMATDATETIME(DATE_D ,'yyyy-MM-dd')>=?  AND FORMATDATETIME(DATE_F ,'yyyy-MM-dd') <=?  ");
+            //PreparedStatement pstmt =connection1.prepareStatement("select DATE_D, DATE_F, BATIMENT,NUM,VIDEO_P,NOM,NATURE from SALLE join CRENEAUX ON(SALLE.ID_S=CRENEAUX.ID_S) join GROUP_COURS ON (CRENEAUX.ID_G=GROUP_COURS.ID_G)join COURS ON (GROUP_COURS.ID_C = COURS.ID_C) join GROUPE on (CRENEAUX.ID_G = GROUPE.ID_G) where GROUPE.LOGIN =? AND FORMATDATETIME(DATE_D ,'yyyy-MM-dd')>=?  AND FORMATDATETIME(DATE_F ,'yyyy-MM-dd') <=?  ");
+
+            pstmt.setString(1,LoginController.user1);
+            name.setText("User: "+LoginController.user1);
+            name.setTextFill(Color.rgb(255, 255, 255));
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            pstmt.setDate(2, java.sql.Date.valueOf(df.format(getmonday().getTime())));
+            System.out.println(df.format(getmonday().getTime()));
+            pstmt.setDate(3, java.sql.Date.valueOf(df.format(getsunday().getTime())));
+            ResultSet queryResult = pstmt.executeQuery();
+            //System.out.println(queryResult.getInt(1));
+            while ((queryResult.next())) {
+                creneau[i][0] = String.valueOf(queryResult.getTimestamp("DATE_D"));
+                creneau[i][1]= String.valueOf(queryResult.getTimestamp("DATE_F"));
+                creneau[i][2] = queryResult.getString("BATIMENT");
+                creneau[i][3] = String.valueOf(queryResult.getInt("NUM"));
+                creneau[i][4] = String.valueOf(queryResult.getBoolean("VIDEO_P"));
+                creneau[i][5] = queryResult.getString("NOM");
+                creneau[i][6] = queryResult.getString("NATURE");
+                i++;
+            }
+
+            System.out.println(Arrays.deepToString(creneau));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+        @FXML
+    public void drawrect() throws ParseException { //fonction qui dessine
+
+
+
+    Calendar x = Calendar.getInstance();
+
+
+    for (int r = 0;r<=i-1;r++) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+       // System.out.println(now.get());
+
+
+        Label cours = new Label();
+
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(creneau[r][0]);
+        Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(creneau[r][1]);
+        //System.out.println(date1);
+        x.setTime(date1);
+        int dayOfWeek = x.get(x.DAY_OF_WEEK);
+        datetopxl(dayOfWeek);
+        double hourofday = x.get(x.HOUR_OF_DAY) + (float) x.get(x.MINUTE) / 60;
+
+        int z = datetopxl(dayOfWeek);
+        double y = houretopxl(hourofday);
+        x.setTime(date2);
+        double hourofday2 = x.get(x.HOUR_OF_DAY) + (float) x.get(x.MINUTE) / 60;
+        System.out.println(hourofday2);
+        double w = houretopxl(hourofday2) - y;
+        System.out.println(w);
+
+        cours.setTranslateX(z);
+        cours.setTranslateY(y);
+        cours.setMinWidth(126);
+        cours.setMinHeight(w);
+        cours.setText(creneau[r][2] +"\n"+  creneau[r][3]+"\n"+ creneau[r][4]+"\n"+ creneau[r][5]+"\n"+ creneau[r][6] +"\n");
+
+        cours.setTextFill(Color.rgb(255, 255, 255));
+        cours.setTextAlignment(TextAlignment.CENTER);
+        cours.setAlignment(Pos.CENTER);
+        System.out.println(creneau[r][6]);
+
+        if(creneau[r][6].trim().equals("TP"))cours.setBackground(new Background(new BackgroundFill(Color.rgb(50, 18, 71), CornerRadii.EMPTY, Insets.EMPTY)));
+        else if(creneau[r][6].trim().equals("TD"))cours.setBackground(new Background(new BackgroundFill(Color.rgb(5, 52, 14), CornerRadii.EMPTY, Insets.EMPTY)));
+        else if(creneau[r][6].trim().equals("CM"))cours.setBackground(new Background(new BackgroundFill(Color.rgb(26, 31, 38), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        scene1.getChildren().add(cours);
+        //scene1.getChildren().addAll(t);
+    }
+
+    }
+
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private  Label loginmessage;
+
+    @FXML
+    private TextField usernametxt;
+    @FXML
+    private  PasswordField passwrdtxt;
+
+}
--- target/classes/create.H2.sql
+++ target/classes/create.H2.sql
@@ -1,29 +1,165 @@
-create table USERACCOUNTS
+
+create table if not exists  UTILISATEUR
 (
-    ID        INT auto_increment,
-    FIRSTNAME VARCHAR       not null,
-    LASTNAME  VARCHAR       not null,
-    USERNAME  VARCHAR       not null,
-    PASSWORD  VARCHAR(5000) not null,
-    ROLE      VARCHAR       not null,
-    constraint USERACCOUNTS_PK
-        primary key (ID)
+    LOGIN    CHAR(15) not null,
+    PASSWORD char not null,
+    NOM      CHAR(30),
+    PRENOM   CHAR(30),
+    EMAIL    CHAR(50),
+    constraint UTILISATEUR_PK
+        primary key (LOGIN)
 );
 
-create unique index USERACCOUNTS_ID_UINDEX
-    on USERACCOUNTS (ID);
+create table if not exists RESPONSABLE
+(
+    LOGIN CHAR(15) not null,
+    constraint RESPONSABLE_PK
+        primary key (LOGIN),
+    constraint RESPONSABLE_UTILISATEUR_LOGIN_FK
+        foreign key (LOGIN) references UTILISATEUR (LOGIN)
+            on update cascade on delete cascade
+);
 
-create unique index USERACCOUNTS_USERNAME_UINDEX
-    on USERACCOUNTS (USERNAME);
+create table if not exists ENSEIGNANT
+(
+    LOGIN CHAR(15) not null,
+    constraint ENSEIGNANT_PK
+        primary key (LOGIN),
+    constraint ENSEIGNANT_UTILISATEUR_LOGIN_FK
+        foreign key (LOGIN) references UTILISATEUR (LOGIN)
+            on update cascade on delete cascade
+);
 
-insert INTO USERACCOUNTS (FIRSTNAME, LASTNAME, Username, PASSWORD,ROLE) VALUES ( 'NEO' ,'ANDERSON','MATRIX','ABCD','PROF');
-insert INTO USERACCOUNTS (FIRSTNAME, LASTNAME, Username, PASSWORD,ROLE) VALUES ( 'UHG' ,'DDD','FFF','VDS','ETU' );
 
-insert INTO USERACCOUNTS (FIRSTNAME, LASTNAME, USERNAME, PASSWORD,ROLE) VALUES ( 'abc' ,'bbbb','cccc','dddd','RES' );
+CREATE TABLE if not exists FILIERE
+(
+    ID_F INT auto_increment,
+    NOM CHAR(30),
+    constraint FILIERE_PK
+        primary key (ID_F)
+);
+
+create table ETUDIANT
+(
+    LOGIN     CHAR(15) not null unique ,
+    NVX_ETUDE CHAR(15),
+    PROMO     CHAR(15),
+    ID_F   int,
+    constraint ETUDIANT_PK
+        primary key (LOGIN),
+    constraint ETUDIANT_FILIERE_FK
+        foreign key (ID_F) references FILIERE(ID_F),
+    constraint ETUDIANT_UTILISATEUR_FK
+        foreign key (LOGIN) references UTILISATEUR
+            on update cascade on delete cascade
+);
 
 
-SELECT  count(1) from USERACCOUNTS where USERNAME= 'cccc' AND PASSWORD = HASH('SHA256', 'dddd', 1000);
 
-UPDATE USERACCOUNTS SET PASSWORD = HASH('SHA256', PASSWORD, 1000);
 
-SELECT count(1) from USERACCOUNTS where USERNAME= 'MATRIX' AND PASSWORD = HASH('SHA256', 'ABCD', 1000);
+
+CREATE TABLE if not exists SALLE(
+                                    ID_S int not null auto_increment,
+                                    NUM CHAR(15) not null ,
+                                    BATIMENT CHAR(15),
+                                    VIDEO_P BOOLEAN,
+                                    constraint SALLE_PK
+                                        primary key (NUM),
+                                    constraint salle_u unique(BATIMENT,NUM)
+);
+
+CREATE TABLE if not exists COURS (
+                                     ID_C INT NOT NULL AUTO_INCREMENT,
+                                     NATURE CHAR(15),
+                                     NOM CHAR(15),
+                                     LOGIN CHAR(15) not null,
+                                     constraint COURS_PK
+                                         primary key (NOM,LOGIN,NATURE),
+                                     constraint COURS_ENSEIGNANT
+                                         foreign key (LOGIN) references ENSEIGNANT(LOGIN),
+                                     CONSTRAINT COURS_U unique (ID_C)
+);
+
+CREATE TABLE if not exists COURS_FILIERE(
+                                            ID_C CHAR(15) not null ,
+                                            ID_F INT not null ,
+                                            TAUX_H NUMERIC(6,2),
+                                            constraint COURS_FILIERE_PK
+                                                primary key (ID_C,ID_F),
+                                            constraint COURS_FILIERE_FK1
+                                                foreign key (ID_C) references COURS(ID_C),
+                                            constraint COURS_FILIERE_FK2
+                                                foreign key (ID_F) references FILIERE(ID_F)
+);
+
+CREATE TABLE if not exists GROUPE (
+                                      ID_G INT not null ,
+                                      LOGIN char(15) not null ,
+                                      constraint GROUP_PK
+                                          primary key  (ID_G,LOGIN),
+                                      constraint GROUP_FK
+                                          foreign key (LOGIN) references ETUDIANT(LOGIN)
+);
+
+CREATE TABLE if not exists GROUP_COURS (
+                                           ID_G INT not null,
+                                           ID_C INT,
+                                           constraint GROUP_COURS_PK
+                                               primary key (ID_G),
+                                           constraint GROUP_COURS_FK1
+                                               foreign key  (ID_G) references GROUPE(ID_G),
+                                           constraint GROUP_COURS_FK2
+                                               foreign key  (ID_C) references COURS(ID_C)
+);
+
+CREATE TABLE if not exists GROUP_FILIERE(
+                                            ID_G INT,
+                                            ID_F INT,
+                                            constraint GROUP_FILIERE_PK
+                                                primary key (ID_G),
+                                            constraint GROUP_FILIERE_FK1
+                                                foreign key  (ID_G) references GROUPE(ID_G),
+                                            constraint GROUP_FILIERE_FK2
+                                                foreign key  (ID_F) references FILIERE(ID_F)
+);
+
+create table if not exists CRENEAUX
+(
+    DATE_D DATETIME not null,
+    DATE_F DATETIME,
+    ID_S   int not null,
+    ID_G INT      not null,
+    constraint CRENAUX_PK
+        primary key (DATE_D, ID_S, ID_G),
+    constraint CRENAUX_GROUPE_ID_G_FK
+        foreign key (ID_G) references GROUPE (ID_G),
+    constraint CRENAUX_SALLE_ID_FK
+        foreign key (ID_S) references SALLE(ID_S)
+);
+
+
+insert INTO UTILISATEUR (NOM, PRENOM, LOGIN, PASSWORD) VALUES ( 'NEO' ,'ANDERSON','MATRIX','ABCD');
+insert INTO UTILISATEUR (NOM, PRENOM, LOGIN, PASSWORD) VALUES ( 'UHG' ,'DDD','FFF','VDS' );
+
+insert INTO UTILISATEUR (NOM, PRENOM, LOGIN, PASSWORD) VALUES ( 'abc' ,'bbbb','cccc','dddd' );
+insert into RESPONSABLE (login)values ( 'cccc' );
+insert into ENSEIGNANT(login)values('FFF');
+insert into FILIERE(NOM) values ( 'DID' );
+insert into ETUDIANT (nvx_etude, promo, login, ID_F) values ( 'master 1','2021/2022','MATRIX',1 );
+insert into salle (ID_S,NUM, video_p) values ( 1,'001',true );
+insert into COURS(LOGIN, nature, nom) values ( 'FFF','CM','java' );
+insert into COURS_FILIERE (id_c, id_f, taux_h) values ( 1,1,22.5 );
+insert into GROUPE(ID_G,LOGIN) values (1,'MATRIX');
+insert into GROUP_COURS(ID_G, ID_C) values ( 1,1 ) ;
+
+
+UPDATE UTILISATEUR SET PASSWORD = HASH('SHA256', PASSWORD, 1000);
+
+
+select DATE_D, DATE_F, BATIMENT,NUM,VIDEO_P,NOM,NATURE from SALLE join CRENEAUX ON(SALLE.ID_S=CRENEAUX.ID_S) join GROUP_COURS ON (CRENEAUX.ID_G=GROUP_COURS.ID_G) join COURS ON (GROUP_COURS.ID_C = COURS.ID_C)
+where LOGIN ='MATRIX' AND DATE_D>=? AND DATE_F <=?;
+
+select DATE_D, DATE_F, BATIMENT,NUM,VIDEO_P,NOM,NATURE from SALLE join CRENEAUX ON(SALLE.ID_S=CRENEAUX.ID_S) join GROUP_COURS ON (CRENEAUX.ID_G=GROUP_COURS.ID_G)join COURS ON (GROUP_COURS.ID_C = COURS.ID_C) join GROUPE on CRENEAUX.ID_G = GROUPE.ID_G
+where GROUPE.LOGIN ='MATRIX' AND DATE_D>=? AND DATE_F <=?;/*GET SYSTEM DATE calender get monday =<0*/
+
+
