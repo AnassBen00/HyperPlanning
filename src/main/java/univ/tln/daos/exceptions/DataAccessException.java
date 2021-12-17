98c6f5292f7bbcb07848730acac789d0c3c024da
acho25 <you@example.com>
2021-12-17 12:57:37 +0100

    Resolution de probleme qu'on avait concernant maven et l'ajout d'une fonction qui affiche les salles correpond à une batiment

---
+++ src/main/java/univ/tln/daos/exceptions/DataAccessException.java
@@ -0,0 +1,7 @@
+package univ.tln.daos.exceptions;
+
+public class DataAccessException extends Exception {
+    public DataAccessException(String localizedMessage) {
+        super(localizedMessage);
+    }
+}
