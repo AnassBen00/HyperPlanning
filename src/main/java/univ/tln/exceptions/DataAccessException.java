9725ee2b5d803be5a9750d421a1f76c44e73a490
unknown <ahmedaitoufqir476@gmail.com>
2021-12-27 21:32:59 +0100

    fixing shit

---
+++ src/main/java/univ/tln/exceptions/DataAccessException.java
@@ -0,0 +1,7 @@
+package univ.tln.exceptions;
+
+public class DataAccessException extends Exception {
+    public DataAccessException(String localizedMessage) {
+        super(localizedMessage);
+    }
+}
