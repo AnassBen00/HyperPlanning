12966c4217074737ca15ec9fdcf897e6618ae101
ait oufqir <ahmedaitoufqir476@gmail.com>
2022-01-02 03:44:24 +0100

    deux fichiers j'ai oublie de l'ajouter dans le commit precedent

---
+++ src/main/java/univ/tln/passEditController.java
@@ -0,0 +1,68 @@
+package univ.tln;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import univ.tln.daos.EnseignantDAO;
+import univ.tln.daos.EtudiantDAO;
+import univ.tln.daos.ResponsableDAO;
+
+public class passEditController {
+
+    @FXML
+    private Button Applybtn;
+
+    @FXML
+    private Button clbtn;
+
+    @FXML
+    private PasswordField newPassField;
+
+    @FXML
+    private PasswordField oldPassField;
+
+    @FXML
+    private Label errormsg;
+
+
+    @FXML
+    void ApplybtnOnAction(ActionEvent event) {
+        if(oldPassField.getText().isBlank() == false && newPassField.getText().isBlank() == false ) {
+            validePassword();
+
+
+        } else {
+            errormsg.setText("Please enter the old and the new password");
+        }
+
+
+    }
+
+    void validePassword() {
+        EnseignantDAO enseignantDAO = new EnseignantDAO();
+        EtudiantDAO etudiantDAO = new EtudiantDAO();
+        ResponsableDAO responsableDao = new ResponsableDAO();
+
+        if(enseignantDAO.checkEnseignantPass(oldPassField.getText())) {
+            Stage stage = (Stage) clbtn.getScene().getWindow();
+            stage.close();
+
+        } else if(etudiantDAO.checkEtudiantPass(oldPassField.getText())) {
+            Stage stage = (Stage) clbtn.getScene().getWindow();
+            stage.close();
+
+        } else if(responsableDao.checkResPass(oldPassField.getText())) {
+            Stage stage = (Stage) clbtn.getScene().getWindow();
+            stage.close();
+
+        } else {
+            errormsg.setText("Old password unvalid try again");
+        }
+
+    }
+
+
+}
